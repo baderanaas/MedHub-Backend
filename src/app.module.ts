5f760707743cdd55f31e996c627e69473e6270fb
asma <asma.zguem@insat.ucar.tn>
2025-01-28 12:17:15 +0100

    Add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,53 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Appointment } from './appointment/entity/appointment.entity';
+import { AppointmentModule } from './appointment/appointment.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { PatientModule } from './patient/patient.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { AuthModule } from './auth/auth.module';
+import { Patient } from './patient/entities/patient.entity';
+import { Doctor } from './doctor/entities/doctor.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASS'),
+        database: configService.get<string>('POSTGRES_DB'),
+        synchronize: true,
+        entities: [Appointment, Patient, Doctor],
+        autoLoadEntities: true,
+      }),
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+
+    AppointmentModule,
+
+    UserModule,
+    PatientModule,
+    DoctorModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: false,
+      exceptionFactory: (errors) =>
+        errors.map((error) => ({
+          field: error.property,
+          messages: Object.values(error.constraints),
+        })),
+    }),
+  );
+  app.enableCors({
+    origin: 'http://localhost:4200', // The URL of your Angular frontend
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (if needed, like cookies or authorization headers)
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
